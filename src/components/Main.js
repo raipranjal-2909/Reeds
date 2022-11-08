597e266fd244962024b9e01648923c7df72649f6
Ashish Chaudhary <ashish080303@gmail.com>
2022-11-09 01:25:53 +0530

    main page progressed

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,8 +7,8 @@ export default function Main() {
     <div className="content">
         <img src={require("../bg-image.png")} alt="" />
         <div className="content-info">
-          <h1 className='Main-heading'>Let US Acheive ur <br /> Dream Deal! </h1>
-          <p className='heading-para'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Esse reiciendis illo tempora aliquam provident debitis.</p>
+          <h1 className='Main-heading'>Let us help you acheive your <br /> Dream Deal! </h1>
+          <p className='heading-para'>Let us help you achieve, sit amet consectetur adipisicing elit. <br /> Esse reiciendis illo tempora aliquam provident debitis.</p>
           <Link to="SignUp"><button>Get Started</button></Link>        
 
         </div>
